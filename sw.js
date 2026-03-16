5b54fb3caf0513a6f9d6090d637bb20e9987592e
boukhobza32saif-blip <boukhobza32saif@gmail.com>
2026-03-16 19:19:43 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE = 'focuslab-v1';
+const FILES = [
+  '/focuslab-habits/',
+  '/focuslab-habits/index.html'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
